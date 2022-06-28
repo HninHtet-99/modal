b42fb813b822ae2d8cb3a4404b9507e510a6c069
HninHtet-99 <hninhtet1113@gmail.com>
2022-06-28 18:09:40 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello World!</h1>
+  
+  <!-- <Modal></Modal> -->
+  <div v-if="showModal">
+  
+    <Modal :header="header" :content="content" :theme="theme" @close="showModal=false">
+
+      <h3>Simple Slot</h3>
+      <template v-slot:namedSlot>
+        <h4>Named Slot</h4>
+      </template>
+    </Modal>
+
+  </div> 
+
+  <teleport to=".teleport-part">
+    <h1>Teleport Part</h1>
+  </teleport>
+  <button @click="showModal=true">show modal</button>
+
+  <!-- delete Modal Start-->
+  <div v-if="delModal" >
+
+    <Modal :delHeader="delHeader" :delContent="delContent" theme="delete" @close="delModal=false"/>
+
+  </div> 
+  <button @click="delModal=true">delete modal</button>
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal-modal.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      header: "Login Success",
+      content: "Hello World!",
+      theme: "success",
+      showModal: false,
+      delModal: false,
+      delHeader: "Delete Modal",
+      delContent: "Are you sure to delete?"
+      
+    }
+  },
+  components:{
+    // Modal : Modal
+    Modal,
+    
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h1{
+    color: aquamarine;
+  }
 </style>
---
+++ src/components/Modal-modal.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="backdrop" @click.self="hideModal">
+
+    <div class="modal" :class="{green : theme ==='success', red : theme === 'delete'}">
+        <div v-if="theme === 'success'">
+
+            <h1 >{{header}}</h1>
+            <p>{{content}}</p>
+            <!-- <slot/> -->
+            <slot>
+                default slot
+            </slot>
+            <slot name="namedSlot"/>
+
+        </div>
+        
+        <div v-else>
+            <h1>{{delHeader}}</h1>
+            <p>{{delContent}}</p>
+        </div>
+        
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    props:["header","content","theme",'delHeader',"delContent"],
+    methods:{
+        hideModal(){
+            this.$emit("close"); 
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .backdrop{
+        position: fixed;
+        top: 0;
+        background: rgb(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100vh;
+    }
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: #f5f5f5;
+        border-radius: 10px;
+    }
+    p{
+        font-style: normal;
+    }
+    .green{
+        background-color: green;
+        color: white;
+    }
+    .red{
+        background-color: red;
+        color: white;
+    }
+</style>
